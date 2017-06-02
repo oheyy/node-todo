8b3cd971f37b17e3aa2eb4d401bd16e973afd0d5
unknown <daniel.seunghyun.oh@gmail.com>
2017-06-02 23:33:16 +1000

    Update config to use json file

--- server/Models/user.js
+++ server/Models/user.js
@@ -64,7 +64,7 @@ userSchema.statics.findByToken = function(token){
     var User = this;
     var decoded;
     try{
-        decoded = jwt.verify(token, "abc123");
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     }catch(e){
         return Promise.reject();
     }
@@ -107,7 +107,7 @@ userSchema.methods.generateAuthToken = function(){
     //in the current user model whereby all except tokens are saved
     var user = this;
     var access  = "auth";
-    var token = jwt.sign({_id: user._id.toHexString(), access}, "abc123");
+    var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET);
 
     user.tokens.push({
         access, 
--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,15 @@
 //If using mocha test NODE_ENV is test if heroku production then else development
 var env = process.env.NODE_ENV|| "development";
 
-if(env === "test"){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest"
-}
-else if(env === "development"){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp"
+if(env === 'development'|| env === 'test'){
+    //Converts from JSON to js object
+    var config = require("./config.json");
+    //To use variable to access use bracket either test/development
+    var envConfig = config[env];
+    //Returns as an array
+    Object.keys(envConfig).forEach((key)=>{
+        //Key == PORT && MONGODB_URI && JWT_SECRET
+        process.env[key] = envConfig[key];
+    });
+
 }
